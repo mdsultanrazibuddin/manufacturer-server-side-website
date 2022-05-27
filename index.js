8e7ea342286928ab470745f8b7e4a8ffaffeced2
mdsultanrazibuddin <mdsultanrazibuddin@gmail.com>
2022-05-27 17:01:17 +0600

    added

--- index.js
+++ index.js
@@ -62,6 +62,13 @@ async function run(){
            
           res.send(users)})
 
+          app.get('/admin/:email', async(req, res) =>{
+            const email = req.params.email;
+            const user = await userCollection.findOne({email: email});
+            const isAdmin = user.role === 'admin';
+            res.send({admin: isAdmin})
+          })
+
       
        
         
